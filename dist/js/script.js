0802430abf6e6c7e6c426bb6d47e686416b8717c
Eric Tseng <amperic05@cengguanyudeMacBook-Pro.local>
2021-03-12 00:32:50 -0800

    forcast page updated, problem with forcast api key

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,13 +1,16 @@
-
 let cityName = 'vancouver';
+const forcastDays = '7';
+const apiKey = '1bfe440130f8c34a9a26a845a5c49429';
+
+fetchWeatherData(cityName);
 
 function searchCity(){
     cityName = document.getElementById('city').value;
-    console.log(cityName);
+    fetchWeatherData(cityName);
 }
 
-setInterval(()=>{
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=1bfe440130f8c34a9a26a845a5c49429`)
+function fetchWeatherData(cityName){
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`)
     .then(res => {
         if(!res){
             console.log('Somthing Wrong!');
@@ -15,13 +18,16 @@ setInterval(()=>{
 
         res.json()
             .then(data => {
-                console.log(data.message);//reference
+                console.log(data);//reference
 
 
                 $(document).ready(()=>{
                     if(data.cod == 200){
                         $('#mainCity').empty()
-                        $('#mainCity').append(`<h3>${data.name}</h3><div><h4>${data.weather[0].main}</h4><h6>${data.weather[0].description}</h6></div><div><h5>Temperature: ${data.main.temp}</h5><h5>Humidity: ${data.main.humidity}</h5><h5>Pressure: ${data.main.pressure}</h5></div>`)
+
+                        let celsius = data.main.temp - 273.15;
+
+                        $('#mainCity').append(`<h3>${data.name}</h3><div><h4>${data.weather[0].main}</h4><h6>${data.weather[0].description}</h6></div><div><h5>Temperature: ${celsius.toFixed(2)} \u00B0C</h5><h5>Humidity: ${data.main.humidity} %</h5><h5>Pressure: ${data.main.pressure} hPa</h5></div>`)
                     }else{
                         $('#mainCity').empty()
                         $('#mainCity').append(`<h3>${data.message}</h3>`)
@@ -30,5 +36,41 @@ setInterval(()=>{
                 })
             })
     })
-}, 2000) 
+}
 
+function fetchForcast(cityName){
+    fetch(`http://api.openweathermap.org/data/2.5/forecast/daily?q=${cityName}&cnt=${forcastDays}&appid=${apiKey}`)
+    .then(res => {
+        if(!res){
+            console.log('Somthing Wrong!');
+        }
+
+        res.json()
+            .then(data => {
+                // console.log(data);//reference
+
+
+                $(document).ready(()=>{
+                    if(data.cod == 200){
+                        $('#weatherForcast').empty()
+                        $('#weatherForcast').append(`<h3>${data.name}</h3><div><h4>${data.weather[0].main}</h4><h6>${data.weather[0].description}</h6></div><div><h5>Temperature: ${data.main.temp}</h5><h5>Humidity: ${data.main.humidity}</h5><h5>Pressure: ${data.main.pressure}</h5></div>`)
+                    }else{
+                        $('#weatherForcast').empty()
+                        $('#weatherForcast').append(`<h3>${data.message}</h3>`)
+                    }
+                        
+                })
+            })
+    })
+}
+
+
+
+
+
+setInterval(()=>{
+    fetchWeatherData(cityName);
+    console.log("Weather update")
+
+    fetchForcast(cityName);
+}, 2000) 
