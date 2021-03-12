f671307849995f2ce7ace64afdbb9af8d58a6608
Eric Tseng <amperic05@cengguanyudeMacBook-Pro.local>
2021-03-11 23:59:32 -0800

    current page function updated

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,4 +1,13 @@
-fetch('http://api.openweathermap.org/data/2.5/weather?q=Vancouver&appid=1bfe440130f8c34a9a26a845a5c49429')
+
+let cityName = 'vancouver';
+
+function searchCity(){
+    cityName = document.getElementById('city').value;
+    console.log(cityName);
+}
+
+setInterval(()=>{
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=1bfe440130f8c34a9a26a845a5c49429`)
     .then(res => {
         if(!res){
             console.log('Somthing Wrong!');
@@ -6,15 +15,20 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=Vancouver&appid=1bfe4401
 
         res.json()
             .then(data => {
-                console.log(data);//reference
+                console.log(data.message);//reference
+
 
                 $(document).ready(()=>{
-
-                    setInterval(()=> {
+                    if(data.cod == 200){
                         $('#mainCity').empty()
                         $('#mainCity').append(`<h3>${data.name}</h3><div><h4>${data.weather[0].main}</h4><h6>${data.weather[0].description}</h6></div><div><h5>Temperature: ${data.main.temp}</h5><h5>Humidity: ${data.main.humidity}</h5><h5>Pressure: ${data.main.pressure}</h5></div>`)
-
-                    }, 2000) 
+                    }else{
+                        $('#mainCity').empty()
+                        $('#mainCity').append(`<h3>${data.message}</h3>`)
+                    }
+                        
                 })
             })
     })
+}, 2000) 
+
